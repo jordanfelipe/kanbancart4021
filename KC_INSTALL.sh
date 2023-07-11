8b77ab0b0e2f628f1aab671e727a1b87c326fc28
Jordan Felipe <jordanfelipe402@gmail.com>
2023-07-10 19:02:21 -0700

    HTTPS working and added downloaded modifications.

--- KC_INSTALL.sh
+++ KC_INSTALL.sh
@@ -2,14 +2,17 @@
 sudo rm -r /var/www/html/
 sudo rm -r /var/www/storage-main/
 sudo mkdir /var/www/html/
-sudo cp -r ~/tmp/upload/* /var/www/html/
+sudo cp -r ~/Documents/KanbanCart/kanbancart4021/upload/* /var/www/html/
+#sudo cp -r ~/tmp/upload/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
     sudo find /var/www/html/ -type d -exec chmod 770 {} +;
     sudo find /var/www/html/ -type d -execdir setfacl -m u:jordan:rwx -m o::--- {} +;
     sudo find /var/www/html/ -type f -exec chmod 660 {} +;
     sudo find /var/www/html/ -type f -exec setfacl -m u:jordan:rw -m o::--- {} +;
     sudo find /var/www/html/ -type d -exec chmod g+s {} +;
+mv /var/www/html/config-dist.php /var/www/html/config.php
 chmod 770 /var/www/html/config.php
+mv /var/www/html/admin/config-dist.php /var/www/html/admin/config.php
 chmod 770 /var/www/html/admin/config.php
 
 # Install Opencart.
@@ -28,3 +31,13 @@ nano /var/www/html/admin/config.php
     # define('DIR_STORAGE', '/var/www/storage-main/');
     # define('HTTP_SERVER', 'http://kanbancart.com/socuda2538/');
 mv /var/www/html/admin /var/www/html/socuda2538
+
+sudo find /var/www/storage-main/ -type d -exec chmod 770 {} +;
+sudo find /var/www/storage-main/ -type d -execdir setfacl -m u:jordan:rwx -m o::--- {} +;
+sudo find /var/www/storage-main/ -type f -exec chmod 660 {} +;
+sudo find /var/www/storage-main/ -type f -exec setfacl -m u:jordan:rw -m o::--- {} +;
+sudo find /var/www/storage-main/ -type d -exec chmod g+s {} +;
+
+sudo find /var/www/html/socuda2538/ -type d -execdir setfacl -m u:jordan:rwx -m o::--- {} +;
+sudo find /var/www/html/socuda2538/ -type f -exec setfacl -m u:jordan:rw -m o::--- {} +;
+sudo find /var/www/html/socuda2538/ -type d -exec chmod g+s {} +;
